9285aaba24b9de926edd17c734570c3992d423ff
Joseph Hilsberg <joqhilsberg@gmail.com>
2016-11-29 15:24:58 +1100

    Added basic directions routing for 3 routes

--- analysis.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("vehicles.csv", sep=',', error_bad_lines=False, index_col=False, dtype='unicode')
-mean = df['year'].mean()
-print('%.3f' % mean)
--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import (Flask, render_template, request, redirect, url_for)
 # from flask_googlemaps import GoogleMaps
 # from flask_googlemaps import Map
 
+import geopy
+from geopy.geocoders import Nominatim
+
 app = Flask(__name__)
 app.debug = True
 
-#app.config['GOOGLEMAPS_KEY'] = "AIzaSyBPq_aC_7y3PA4Bim15wIhe8RFRguXEjzw"
-# GoogleMaps(app)
+geolocator = Nominatim()
 
 @app.route("/google2e4c61498b4fdd7f.html")
 def googlecheck():
@@ -19,49 +21,21 @@ def index():
 @app.route("/routesMap", methods=['GET', 'POST'])
 def routeMap():
     dest = {}
-    dest['from'] = ''
-    dest['to'] = ''
+    dest['from'] = None
+    dest['to'] = None
     if (request.method == "POST"):
         try:
-            dest['from'] = request.form['destFrom']
-            dest['to'] = request.form['destTo']
+            fromLoc = geolocator.geocode(request.form['destFrom'])
+            toLoc = geolocator.geocode(request.form['destTo'])
+            dest['from'] = fromLoc
+            dest['to'] = toLoc
         except:
-            pass
+            print "Failed to read off input"
+            return redirect(url_for('index'))
         return render_template("routesMap.html", dest=dest)
     else:
-        return render_template("routesMap.html", dest=dest)
-
-'''
-@app.route("/test")
-def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
-    )
-    return render_template('example.html', mymap=mymap, sndmap=sndmap)
-'''
+            print "Page not loaded with POST"
+            return redirect(url_for('index'))
 
 if (__name__ == "__main__"):
     app.run(debug=True)
